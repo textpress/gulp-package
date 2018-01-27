f5a7e55bce5e518401fe1b7d637442c124c9352c
Aleksey Gurtovoy <agurtovoy@acm.org>
2018-01-26 22:29:40 -0600

    Chore: cleanup (an easier way to include hidden files)

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import zip from "gulp-zip";
 
 
 export function packageBuild( { buildFolder, packageFolder, packageFile } ) {
-    // see https://github.com/klaascuvelier/gulp-copy/issues/5
-    const everything = [ "**/*", "**/.*", ".**/*", ".**/.*" ].map( pattern => `${buildFolder}/${pattern}` );
-    return gulp.src( everything, { buffer: false } )
+    return gulp.src( `${buildFolder}/**/*`, { dot: true, buffer: false } )
         .pipe( zip( packageFile ) )
         .pipe( gulp.dest( packageFolder ) );
 }
